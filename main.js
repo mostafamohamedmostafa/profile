694391fcdf304572ddab6e4f9d66a154be8a1e93
mostafamohamedmostafa <54216066+mostafamohamedmostafa@users.noreply.github.com>
2020-01-02 23:59:30 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+/*const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('nav-menu');
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        showMenu = true;
+    } else {
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        showMenu = false;
+    }
+}
+*/
